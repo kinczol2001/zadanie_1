0f8971520d5a2da5d2f5331feea466aa67ec1142
Filip Kinczewski <fkinczewski@icloud.com>
2024-03-13 11:06:34 +0100

    dodanie reszty figur

--- exercises/exercis1.py
+++ exercises/exercis1.py
@@ -3,3 +3,25 @@ bok_a = 10.5
 pole_kw = bok_a*bok_a
 
 print("Pole kwadratu wynosi: ", pole_kw)
+
+dl_podstawy = 10
+wys = 10
+
+pole_tr = (dl_podstawy*wys)/2
+
+print("Pole trójkąta wynosi: ",pole_tr)
+
+bok_b=10
+bok_c=12
+
+pole_pr=bok_b*bok_c
+
+print("Pole prostokąta wynosi: ",pole_pr)
+
+bok_d=10
+bok_e=20
+bok_f=12
+
+pole_trapezu=(bok_d+bok_e)*bok_f/2
+
+print("Pole trapezu wynosi: ",pole_trapezu)
